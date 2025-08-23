7fc545c619f5bac3d78e05aeb7dc89af1173cf69
Deepak Ram <154036627+Deepakram0929@users.noreply.github.com>
2025-08-23 16:46:27 +0530

    Update Docker-compose.sh

--- Docker-compose.sh
+++ Docker-compose.sh
@@ -1,5 +1,3 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-ls /usr/local/bin/
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose version
+docker-compose --version
